bd0ae4dbb42052da7ef9cba8236143be605d843c
roshan <roshankewat007@gmail.com>
2020-01-14 19:54:12 +0530

    lopgin-signup

--- home1/admin.py
+++ home1/admin.py
@@ -1,11 +1,5 @@
 
 from django.contrib import admin
 
-# from .forms import PatientRegstrationForm
-# Register your models here.
-
-from django.contrib import admin
-
-# admin.site.register(PatientRegstrationForm)
-# Register your models here.
-
+from django.contrib import admin
+
--- home1/views.py
+++ home1/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from sklearn.externals import joblib
+# from sklearn.externals import joblib
 import numpy as np
 # Create your views here.
 from django.contrib import messages
@@ -11,9 +11,46 @@ from django.shortcuts import render, redirect
 def home(request):
     return  render(request,'index.html')
 def login(request):
-    return render(request,'login.html')
-def signup(request):
-    return render(request,'signup.html')
+    if request.method== 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect("/")
+        else:
+            messages.info(request,'invalid credentials')
+            return redirect('login')
+    else:
+        return render(request,'login.html')
+def signup(request): 
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        confirmpassword= request.POST['confirmpassword']
+        email = request.POST['email']
+
+        if password==confirmpassword:
+            if User.objects.filter(username=username).exists():
+                messages.info(request,'Username Taken')
+                return redirect('signup')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request,'Email Taken')
+                return redirect('signup')
+            else:   
+                user = User.objects.create_user(username=username, password=password, email=email,first_name="",last_name="")
+                user.save()
+                print('user created')
+                return redirect('login')
+
+        else:
+            messages.info(request,'password not matching..')    
+            return redirect('signup')
+        return redirect('/')
+        
+    else:
+        return render(request,'signup.html')
+ 
 def register(request):
     if request.method == "POST":
         pname = request.POST['pname']
@@ -33,12 +70,12 @@ def register(request):
         # return HttpResponse("kwehfke")
     else:
         return render(request,'register.html')
-    
-    #return render(request,'register.html')
 
-def predict(request):
-    pklout =  open("C:\\Users\\abc\\.spyder-py3\\predict queue wait time\\randomforest.pkl","rb")
-    knn_from_joblib = joblib.load(pklout)
-    ini_array = np.array([[2, 29, 1]])
-    str2 = knn_from_joblib.predict(ini_array) 
-    return render(request,'predict.html',{'f':str2})
+    
+
+# def predict(request):
+#     pklout =  open("C:\\Users\\abc\\.spyder-py3\\predict queue wait time\\randomforest.pkl","rb")
+#     knn_from_joblib = joblib.load(pklout)
+#     ini_array = np.array([[2, 29, 1]])
+#     str2 = knn_from_joblib.predict(ini_array) 
+#     return render(request,'predict.html',{'f':str2})
